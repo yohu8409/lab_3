7127de467c05a3592510a61b57efe337b8ba1c85
yohu8409 <yohu8409@colorado.edu>
2020-02-06 14:45:06 -0700

    final changes

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,12 +4,14 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
-echo "Please enter filename"
-read name
-echo "Enter regular expression"
-read expression
-grep -c $expression $name
-grep $expression $name >> email.txt
+echo "filename: regex_practice.txt"
+echo "Number of phone numbers"
+egrep -c [0-9]{3}-[0-9]{3}-[0-9]{4} regex_practice.txt
+echo "pushing emails to email.txt"
+egrep -c [a-z]{+}@[a-z]{+} regex_practice.txt
+egrep [a-z]+@[a-z]+ regex_practice.txt >> email.txt
+
+
 
 
 
